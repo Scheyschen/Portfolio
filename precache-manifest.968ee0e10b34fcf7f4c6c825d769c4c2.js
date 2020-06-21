1cd0f3b392e4065a5063696f62a198d3e6c5049e
Scheyschen <scheyschen@gmail.com>
2020-06-21 13:37:54 +0200

    Updates

--- precache-manifest.968ee0e10b34fcf7f4c6c825d769c4c2.js
+++ precache-manifest.968ee0e10b34fcf7f4c6c825d769c4c2.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6015a0083d7c3e349faf60d3189ca1b0",
+    "revision": "3a6bd31f38201583f18b2537b678e78f",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "8eed12a71b1a844816e4",
+    "revision": "7d01c259cfadde2676f6",
     "url": "/portfolio/static/css/main.5f361e03.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.4c736102.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8eed12a71b1a844816e4",
-    "url": "/portfolio/static/js/main.10a1b4a7.chunk.js"
+    "revision": "7d01c259cfadde2676f6",
+    "url": "/portfolio/static/js/main.dfa51eef.chunk.js"
   },
   {
     "revision": "7a6cc99bd8f08f9fa6ec",
